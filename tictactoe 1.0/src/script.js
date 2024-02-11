ba684268ab0ff1a21ef2a9b06d40dd5c52fbea4c
Banyu_sans <banyuarfa1122@gmail.com>
2024-02-11 01:56:32 -0800

    release

--- tictactoe 1.0/src/script.js
+++ tictactoe 1.0/src/script.js
@@ -10,42 +10,61 @@ const winConditions = [
   [3, 5, 7],
 ];
 const winStatus = document.querySelector("p");
-let option = ["", "", "", "", "", "", "", "", ""];
+let option = ["", "", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
+let running = false;
 
-cells.forEach((cell, index) => {
-  cell.addEventListener("click", () => {
-    if (currentPlayer == "X") {
-      cell.textContent = currentPlayer;
-      option[index] = currentPlayer;
-      currentPlayer = "O";
-    } else {
-      cell.textContent = currentPlayer;
-      option[index] = currentPlayer;
+const initializeGame = () => {
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+  running = true;
+};
 
-      currentPlayer = "X";
+const cellClicked = (e) => {
+  const cellIndex = e.target.getAttribute("cellIndex");
+  console.log(cellIndex);
+  if (option[cellIndex] != "" || !running) {
+    return;
+  }
+
+  updateCell(e, cellIndex);
+  checkWinner();
+};
+
+const updateCell = (e, cellIndex) => {
+  option[cellIndex] = currentPlayer;
+  e.target.textContent = currentPlayer;
+};
+const changePlayer = () => {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  winStatus.textContent = `${currentPlayer} jalan`;
+};
+
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const cellA = option[condition[0]];
+    const cellB = option[condition[1]];
+    const cellC = option[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
     }
-    const value = this.getAttribute("value");
-    if (option[value] != "") {
-      return;
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+      break;
     }
-    const roundWin = false;
-    for (let i = 0; i < winConditions.length; i++) {
-      let condition = winConditions[i];
-      let a = option[condition[0]];
-      let b = option[condition[1]];
-      let c = option[condition[2]];
-      if (a == "" || b == "" || c == "") {
-        continue;
-      } else if (a == b && b == c) {
-        roundWin = true;
-        break;
-      }
-    }
-    if (roundWin) {
-      winStatus.textContent = `player ${currentPlayer} win!`;
-    } else if (!option.includes("")) {
-      winStatus.textContent = `draw!`;
-    }
-  });
-});
+  }
+
+  if (roundWon) {
+    winStatus.textContent = `${currentPlayer} wins!`;
+    running = false;
+  } else if (!option.includes("")) {
+    winStatus.textContent = `Draw!`;
+    running = false;
+  } else {
+    changePlayer();
+  }
+}
+initializeGame();
